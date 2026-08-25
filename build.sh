9bae17b784dd924092924c66801db69dbb651623
Yingpeng Tang <1971319091@qq.com>
2026-08-26 00:40:32 +0800

    将MLIR单独进行构建，加快开发编译速度和避免命名空间污染。

--- build.sh
+++ build.sh
@@ -2,11 +2,12 @@
 
 set -e
 cd "$(dirname "$0")" # 进入脚本所在目录
-if test -d ./install; then
-  echo "install 已经存在，确认是否需要重新构建，如果需要请删除build和install目录." >&2
-  exit 1
+if test -d ./third_party/install; then
+  echo "install 已经存在，跳过mlir构建安装" >&2
+else
+  echo "install目录不存在，开始构建mlir..."
+  rm -rf ./third_party/build
+  cmake -B ./third_party/build -S ./third_party
+  cmake --build ./third_party/build --target install -j$(nproc --ignore 2) # 留出2个CPU核心给系统使用
 fi
-cmake -B build
-# 使用install目标是为了把mlir的可执行文件放到./install中。
-cmake --build build --target install -j$(nproc --ignore 2) # 留出2个CPU核心给系统使用
-
+echo "mlir构建安装完成，开始构建项目..."
