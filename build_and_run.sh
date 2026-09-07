3ab98cd12579d8607fc59f397d8bc5dd1eea8f15
Yingpeng Tang <1971319091@qq.com>
2026-09-08 00:29:38 +0800

    构建运行脚本中添加git submodule拉取，确保依赖存在。

--- build_and_run.sh
+++ build_and_run.sh
@@ -6,6 +6,7 @@ if test -d ./third_party/install; then
   echo "install 已经存在，跳过mlir构建安装" >&2
 else
   echo "install目录不存在，开始构建mlir..."
+  git submodule update --init --recursive
   rm -rf ./third_party/build
   cmake -B ./third_party/build -S ./third_party
   cmake --build ./third_party/build --target install -j$(nproc --ignore 2) # 留出2个CPU核心给系统使用
